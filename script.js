5f68bcb722e7d417dc686eda58ab0295857aa9b6
ktsim88 <526ktsim@frhsd.com>
2024-12-02 11:50:25 -0500

    added images and gifs

--- script.js
+++ script.js
@@ -8,23 +8,30 @@ function shakeMagicBall() {
     
     
     let userQuestion = prompt('What is your question for the mighty eightball?')
-    
+    let responseImg = document.getElementById('response-image')
     if (randomNumber === 0) {
       eightBall = 'It is certain'
+      
     } else if (randomNumber === 1) {
         eightBall = 'It is decidely so'
     } else if (randomNumber === 2) {
-      console.log('Reply hazy try again')
+      eightBall = 'Reply hazy try again'
+      responseImg.src = 'imgs/duoBackpack.gif'
     } else if (randomNumber === 3) {
-        console.log('Cannot predict now')
+      eightBall = 'Cannot predict now'
+      responseImg.src = 'imgs/girlShocked.gif'
     } else if (randomNumber === 4) {
-      console.log('Do not count on it')
+      eightBall = 'Do not count on it'
+      responseImg.src = "imgs/DuoAngryIdle.gif"
     } else if (randomNumber === 5) {
-      console.log('My sources say no')
+      eightBall = 'My sources say no'
+      responseImg.src = 'imgs/linIsScared.gif'
     } else if (randomNumber === 6) {
-      console.log('Outlook not so good')
+      eightBall = 'Outlook not so good'
+      responseImg.src = 'imgs/outlookNoBuenoDuo.jpg'
     } else if (randomNumber === 7) {
-      console.log('Signs point to yes')
+      eightBall = 'Signs point to yes'
+      responseImg.src = 'imgs/simpson.jpg'
     }
     console.log(eightBall)
     document.getElementById('response-text').innerText = `${userName} has asked the magic 8 ball ${userQuestion}. The wise 8 ball has replied with ${eightBall}`
